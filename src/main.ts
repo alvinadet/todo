3a86e3d02d941b325876f5db89d7ef8fe943a3c0
alvinadet <alvinadetya@rocketmail.com>
2021-06-21 23:10:22 +0700

    fix: 🐛 env and typeorm modul

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+require('dotenv').config();
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const { PORT } = process.env;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  console.log(`Nest is start on port ${PORT}`);
+  await app.listen(PORT);
 }
 bootstrap();
