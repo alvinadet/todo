a769cf1f59ac5b0fb5f6dabdb96befbf9c38bca0
alvin adetya <alvinadetya@rocketmail.com>
2021-07-19 05:49:18 +0700

    fix: 🐛 jwt strategy

--- src/auth/jwt.guard.ts
+++ src/auth/jwt.guard.ts
@@ -1,5 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { ExecutionContext, Inject, Injectable } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { AuthService } from './auth.service';
 
 @Injectable()
-export class JwtAuthGuard extends AuthGuard('jwt') {}
+export class JwtAuthGuard extends AuthGuard('jwt') {
+  constructor(
+    @Inject('AuthService')
+    private authService: AuthService,
+  ) {
+    super();
+  }
+
+  // canActivate(context: ExecutionContext) {
+  //   const request = context.switchToHttp().getRequest<Request>();
+
+  //   // Read token
+  //   const authorization = request.headers['authorization']?.replace(
+  //     'Bearer ',
+  //     '',
+  //   );
+  //   if (!authorization) {
+  //     return false;
+  //   }
+
+  //   return super.canActivate(context);
+  // }
+}
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,11 +1,22 @@
-import { ExtractJwt, Strategy } from 'passport-jwt';
+import { HttpStatus, Inject, Injectable } from '@nestjs/common';
+import { HttpException } from '@nestjs/common/exceptions';
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ContextProvider } from 'src/context/context.provider';
+import { UserService } from 'src/user/user.service';
 import { jwtConstants } from './auth.constant';
+import { JwtPayload } from './auth.interface';
+import { AuthService } from './auth.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  authService: AuthService;
+  constructor(
+    @Inject(UserService)
+    private userService: UserService,
+    @Inject(ContextProvider)
+    private contextProvider: ContextProvider,
+  ) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -13,7 +24,12 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  async validate(payload: any) {
-    return { id: payload.id, username: payload.username };
+  async validate(payload: JwtPayload) {
+    const user = await this.userService.findOne({ id: payload.id });
+    if (!user) {
+      throw new HttpException('Invalid token', HttpStatus.UNAUTHORIZED);
+    }
+    this.contextProvider.setUser(user);
+    return user;
   }
 }
