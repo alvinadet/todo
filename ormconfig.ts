d95240ce558c9d25c3297b21d8d064bef47f8d14
alvin adetya <alvinadetya@rocketmail.com>
2021-10-25 19:06:15 +0700

    fix: 🐛 orm log

--- ormconfig.ts
+++ ormconfig.ts
@@ -3,6 +3,8 @@ require('dotenv').config();
 
 const { DB_SERVER, DB_PORT, DB_USER, DB_PASSWORD, DB_DATABASE } = process.env;
 
+console.log(process.env)
+
 const config = {
   type: 'postgres',
   host: DB_SERVER,
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,18 +1,28 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
+import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
+import { Connection } from 'typeorm';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    // app.useLogger(new TestLogger()) // more on this line is below
     await app.init();
+    if (process.env.NODE_ENV === 'test') {
+      const connection = app.get(Connection);
+      await connection.synchronize(true);
+    }
+  });
+
+  afterAll(async () => {
+    await app.close();
   });
 
   it('/ (GET)', () => {
